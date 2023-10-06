23d3b9eebf6f6a3d3dd9804c88e738f2219f50db
Katelyn Yaeger <knyaeger93@gmail.com>
2023-10-06 12:54:27 -0500

    initial commit

---
+++ RonvsKanyeAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Net.WebRequestMethods;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What questions do you have for Ron and Kanye?");
+
+            bool answer = true;
+
+            while (true)
+            {
+                var userQuestion = Console.ReadLine();
+                Console.WriteLine($"Kanye: {Ron_KanyeAPI.RonvsKanye.KanyeQuote()}");
+                Console.WriteLine($"Ron: {Ron_KanyeAPI.RonvsKanye.RonQuote()}");
+                Console.WriteLine("Any other questions?");
+
+                var userAnswer = Console.ReadLine();
+
+                if (userAnswer.ToUpper() == "NO")
+                {
+                    answer = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Go ahead.");
+                }
+            }
+        }
+    }
+}
---
+++ RonvsKanyeAPI/RonvsKanye.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ron_KanyeAPI
+{
+    internal class RonvsKanye
+    {
+        public static string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("----");
+
+            return kanyeQuote;
+        }
+
+        public static string RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine("-------");
+
+            return ronQuote;
+        }
+    }
+}
+
+
